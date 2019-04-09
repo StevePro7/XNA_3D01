b1483ee9adc935a8e8d84dd9b89a5a82843ca4ca
StevePro7 <steveprozune@hotmail.com>
2019-04-09 21:47:26 +0100

    Complete chap01

---
+++ MainGame/MainGame/MainGame/Library/DepthOfField.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace MyGame
+{
+    public class DepthOfField : PostProcessor
+    {
+        // Depth map and un-blurred render of scene. The blurred render
+        // will be set as the Input value
+        public Texture2D DepthMap;
+        public Texture2D Unblurred;
+
+        public DepthOfField(GraphicsDevice graphicsDevice, ContentManager Content)
+            : base(Content.Load<Effect>("DepthOfField"), graphicsDevice)
+        {
+        }
+
+        public override void Draw()
+        {
+            // Set the two textures above to the second and third
+            // texture slots
+            graphicsDevice.Textures[1] = Unblurred;
+            graphicsDevice.Textures[2] = DepthMap;
+
+            // Set the samplers for all three textures to PointClamp
+            // so we can sample pixel values directly
+            graphicsDevice.SamplerStates[0] = SamplerState.PointClamp;
+            graphicsDevice.SamplerStates[1] = SamplerState.PointClamp;
+            graphicsDevice.SamplerStates[2] = SamplerState.PointClamp;
+
+            base.Draw();
+        }
+    }
+}
--- MainGame/MainGame/MainGame/Program.cs
+++ MainGame/MainGame/MainGame/Program.cs
@@ -1,6 +1,10 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using Microsoft.Xna.Framework;
 
-namespace MainGame
+namespace MyGame
 {
 #if WINDOWS || XBOX
     static class Program
@@ -10,11 +14,33 @@ namespace MainGame
         /// </summary>
         static void Main(string[] args)
         {
-            using (Game1 game = new Game1())
-            {
-                game.Run();
-            }
+	        int index = Convert.ToInt32(ConfigurationManager.AppSettings["game"]);
+	        IDictionary<int, Game> dictionary = GetGameDictionary();
+	        Game game = null;
+	        try
+	        {
+				game = dictionary[index];
+		        game.Run();
+	        }
+	        finally
+	        {
+		        if (null != game)
+		        {
+			        game.Dispose();
+		        }
+	        }
         }
+
+		private static IDictionary<int, Game> GetGameDictionary()
+		{
+			IDictionary<int, Game> dictionary = new Dictionary<int, Game>();
+			dictionary.Add(1, new Load01Game1());
+			dictionary.Add(2, new Load02Game1());
+			dictionary.Add(3, new Load03Game1());
+			dictionary.Add(4, new Load04Game1());
+			dictionary.Add(5, new Load05Game1());
+			return dictionary;
+		}
     }
 #endif
 }
