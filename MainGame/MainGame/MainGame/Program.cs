f29cea4083ac739a73e2c4d4f7b8ce092d80e003
StevePro7 <steveprozune@hotmail.com>
2019-04-10 16:59:07 +0100

    Update program

--- MainGame/MainGame/MainGame/Program.cs
+++ MainGame/MainGame/MainGame/Program.cs
@@ -34,11 +34,35 @@ namespace MyGame
 		private static IDictionary<int, Game> GetGameDictionary()
 		{
 			IDictionary<int, Game> dictionary = new Dictionary<int, Game>();
-			dictionary.Add(1, new Demo11Game1());
-			dictionary.Add(2, new Demo12Game1());
-			dictionary.Add(3, new Demo13Game1());
-			dictionary.Add(4, new Demo14Game1());
-			dictionary.Add(5, new Demo15Game1());
+
+			// Chap01
+			dictionary.Add(11, new Demo11Game1());
+			dictionary.Add(12, new Demo12Game1());
+			dictionary.Add(13, new Demo13Game1());
+			dictionary.Add(14, new Demo14Game1());
+			dictionary.Add(15, new Demo15Game1());
+
+			// Chap02
+			dictionary.Add(21, new Demo21Game1());
+
+			// Chap03
+			dictionary.Add(31, new Demo31Game1());
+			dictionary.Add(32, new Demo32Game1());
+			dictionary.Add(33, new Demo33Game1());
+			dictionary.Add(34, new Demo34Game1());
+
+			// Chap04
+			dictionary.Add(41, new Demo41Game1());
+			dictionary.Add(42, new Demo42Game1());
+			dictionary.Add(43, new Demo43Game1());
+
+			// Chap05
+			dictionary.Add(51, new Demo51Game1());
+			dictionary.Add(52, new Demo52Game1());
+			dictionary.Add(53, new Demo53Game1());
+			dictionary.Add(54, new Demo54Game1());
+			dictionary.Add(55, new Demo55Game1());
+
 			return dictionary;
 		}
     }
