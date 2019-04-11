d8d27374713d86d90c2e8e56f60fc2fc97b62b48
StevePro7 <steveprozune@hotmail.com>
2019-04-11 16:45:23 +0100

    Update program

--- MainGame/MainGame/MainGame/Program.cs
+++ MainGame/MainGame/MainGame/Program.cs
@@ -64,29 +64,29 @@ namespace MyGame
 			dictionary.Add(55, new Demo55Game1());
 
 			// Chap06
-			dictionary.Add(51, new Demo61Game1());
-			dictionary.Add(52, new Demo62Game1());
-			dictionary.Add(53, new Demo63Game1());
+			dictionary.Add(61, new Demo61Game1());
+			dictionary.Add(62, new Demo62Game1());
+			dictionary.Add(63, new Demo63Game1());
 
 			// Chap07
-			dictionary.Add(51, new Demo71Game1());
-			dictionary.Add(52, new Demo72Game1());
-			dictionary.Add(53, new Demo73Game1());
-			dictionary.Add(54, new Demo74Game1());
+			dictionary.Add(71, new Demo71Game1());
+			dictionary.Add(72, new Demo72Game1());
+			dictionary.Add(73, new Demo73Game1());
+			dictionary.Add(74, new Demo74Game1());
 
 			// Chap08
-			dictionary.Add(51, new Demo81Game1());
-			dictionary.Add(52, new Demo82Game1());
-			dictionary.Add(53, new Demo83Game1());
-			dictionary.Add(54, new Demo84Game1());
-			dictionary.Add(55, new Demo85Game1());
+			dictionary.Add(81, new Demo81Game1());
+			dictionary.Add(82, new Demo82Game1());
+			dictionary.Add(83, new Demo83Game1());
+			dictionary.Add(84, new Demo84Game1());
+			dictionary.Add(85, new Demo85Game1());
 
 			// Chap09
-			dictionary.Add(51, new Demo91Game1());
-			dictionary.Add(52, new Demo92Game1());
-			dictionary.Add(53, new Demo93Game1());
-			dictionary.Add(54, new Demo94Game1());
-			dictionary.Add(55, new Demo95Game1());
+			dictionary.Add(91, new Demo91Game1());
+			dictionary.Add(92, new Demo92Game1());
+			dictionary.Add(93, new Demo93Game1());
+			dictionary.Add(94, new Demo94Game1());
+			dictionary.Add(95, new Demo95Game1());
 			return dictionary;
 		}
     }
