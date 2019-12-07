ec06c73884398b28af89e8930658446cac84a963
StevePro7 <steveprozune@hotmail.com>
2019-12-07 11:23:55 +0000

    no message

---
+++ PortGame/PortGame/Chapter/Chap04/Demo41Game1.cs
@@ -0,0 +1,128 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Input;
+
+namespace MyGame
+{
+    public class Demo41Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        List<CModel> models = new List<CModel>();
+        Camera camera;
+
+        MouseState lastMouseState;
+
+        PrelightingRenderer renderer;
+
+		public Demo41Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            graphics.PreferredBackBufferWidth = 1280;
+            graphics.PreferredBackBufferHeight = 800;
+        }
+
+        // Called when the game should load its content
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            models.Add(new CModel(Content.Load<Model>("teapot"),
+                new Vector3(0, 60, 0), Vector3.Zero, new Vector3(60), 
+                GraphicsDevice));
+
+            models.Add(new CModel(Content.Load<Model>("ground"),
+                Vector3.Zero, Vector3.Zero, Vector3.One, GraphicsDevice));
+
+            Effect effect = Content.Load<Effect>("ProjectedTexture");
+
+            models[0].SetModelEffect(effect, true);
+            models[1].SetModelEffect(effect, true);
+
+            ProjectedTextureMaterial mat = new ProjectedTextureMaterial(
+                Content.Load<Texture2D>("projected texture"), GraphicsDevice);
+            mat.ProjectorPosition = new Vector3(0, 4500, 4500);
+            mat.ProjectorTarget = new Vector3(0, 0, 0);
+            mat.Scale = 2;
+
+            models[0].Material = mat;
+            models[1].Material = mat;
+
+            camera = new FreeCamera(new Vector3(0, 300, 1600),
+                MathHelper.ToRadians(0), // Turned around 153 degrees
+                MathHelper.ToRadians(5), // Pitched up 13 degrees
+                GraphicsDevice);
+
+            renderer = new PrelightingRenderer(GraphicsDevice, Content);
+            renderer.Models = models;
+            renderer.Camera = camera;
+            renderer.Lights = new List<PPPointLight>() {
+                new PPPointLight(new Vector3(-1000, 1000, 0), Color.Red * .85f, 2000),
+                new PPPointLight(new Vector3(1000, 1000, 0), Color.Blue * .85f, 2000),
+            };
+
+            lastMouseState = Mouse.GetState();
+        }
+
+        // Called when the game should update itself
+        protected override void Update(GameTime gameTime)
+        {
+            updateCamera(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        void updateCamera(GameTime gameTime)
+        {
+            // Get the new keyboard and mouse state
+            MouseState mouseState = Mouse.GetState();
+            KeyboardState keyState = Keyboard.GetState();
+
+            // Determine how much the camera should turn
+            float deltaX = (float)lastMouseState.X - (float)mouseState.X;
+            float deltaY = (float)lastMouseState.Y - (float)mouseState.Y;
+
+            // Rotate the camera
+            ((FreeCamera)camera).Rotate(deltaX * .005f, deltaY * .005f);
+
+            Vector3 translation = Vector3.Zero;
+
+            // Determine in which direction to move the camera
+            if (keyState.IsKeyDown(Keys.W)) translation += Vector3.Forward;
+            if (keyState.IsKeyDown(Keys.S)) translation += Vector3.Backward;
+            if (keyState.IsKeyDown(Keys.A)) translation += Vector3.Left;
+            if (keyState.IsKeyDown(Keys.D)) translation += Vector3.Right;
+
+            // Move 3 units per millisecond, independent of frame rate
+            translation *= 4 * 
+                (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            // Move the camera
+            ((FreeCamera)camera).Move(translation);
+
+            // Update the camera
+            camera.Update();
+
+            // Update the mouse state
+            lastMouseState = mouseState;
+        }
+
+        // Called when the game should draw itself
+        protected override void Draw(GameTime gameTime)
+        {
+            renderer.Draw();
+
+            GraphicsDevice.Clear(Color.Black);
+
+            foreach (CModel model in models)
+                if (camera.BoundingVolumeIsInView(model.BoundingSphere))
+                    model.Draw(camera.View, camera.Projection, ((FreeCamera)camera).Position);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ PortGame/PortGame/Chapter/Chap08/Demo83Game1.cs
@@ -0,0 +1,139 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace MyGame
+{
+    public class Demo83Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        List<CModel> models = new List<CModel>();
+        Camera camera;
+
+        MouseState lastMouseState;
+
+        RenderCapture renderCapture;
+        PostProcessor postprocessor;
+
+		public Demo83Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            graphics.PreferredBackBufferWidth = 1280;
+            graphics.PreferredBackBufferHeight = 800;
+        }
+
+        // Called when the game should load its content
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            camera = new FreeCamera(new Vector3(1000, 650, 1000),
+                MathHelper.ToRadians(45),
+                MathHelper.ToRadians(-30),
+                GraphicsDevice);
+
+            Effect effect = Content.Load<Effect>("LightingEffect");
+            LightingMaterial mat = new LightingMaterial();
+
+            for (int z = -1; z <= 1; z++)
+                for (int x = -1; x <= 1; x++)
+                {
+                    CModel model = new CModel(Content.Load<Model>("teapot"),
+                        new Vector3(x * 500, 0, z * 500), Vector3.Zero, 
+                        Vector3.One * 50, GraphicsDevice);
+
+                    model.SetModelEffect(effect, true);
+                    model.SetModelMaterial(mat);
+
+                    model.Model.Meshes[0].MeshParts[0].Effect.Parameters["BasicTexture"].SetValue(
+                        Content.Load<Texture2D>("brick_texture_map"));
+                    model.Model.Meshes[0].MeshParts[0].Effect.Parameters["TextureEnabled"].SetValue(true);
+
+                    models.Add(model);
+                }
+
+            CModel ground = new CModel(Content.Load<Model>("ground"),
+                Vector3.Zero, Vector3.Zero, Vector3.One, GraphicsDevice);
+
+            ground.SetModelEffect(effect, true);
+            ground.SetModelMaterial(mat);
+
+            models.Add(ground);
+
+            renderCapture = new RenderCapture(GraphicsDevice);
+            postprocessor = new GaussianBlur(GraphicsDevice, Content, 2);
+
+            lastMouseState = Mouse.GetState();
+        }
+
+        // Called when the game should update itself
+        protected override void Update(GameTime gameTime)
+        {
+            updateCamera(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        void updateCamera(GameTime gameTime)
+        {
+            // Get the new keyboard and mouse state
+            MouseState mouseState = Mouse.GetState();
+            KeyboardState keyState = Keyboard.GetState();
+
+            // Determine how much the camera should turn
+            float deltaX = (float)lastMouseState.X - (float)mouseState.X;
+            float deltaY = (float)lastMouseState.Y - (float)mouseState.Y;
+
+            // Rotate the camera
+            ((FreeCamera)camera).Rotate(deltaX * .005f, deltaY * .005f);
+
+            Vector3 translation = Vector3.Zero;
+
+            // Determine in which direction to move the camera
+            if (keyState.IsKeyDown(Keys.W)) translation += Vector3.Forward;
+            if (keyState.IsKeyDown(Keys.S)) translation += Vector3.Backward;
+            if (keyState.IsKeyDown(Keys.A)) translation += Vector3.Left;
+            if (keyState.IsKeyDown(Keys.D)) translation += Vector3.Right;
+
+            // Move 4 units per millisecond, independent of frame rate
+            translation *= 0.5f * 
+                (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            // Move the camera
+            ((FreeCamera)camera).Move(translation);
+
+            // Update the camera
+            camera.Update();
+
+            // Update the mouse state
+            lastMouseState = mouseState;
+        }
+
+        // Called when the game should draw itself
+        protected override void Draw(GameTime gameTime)
+        {
+            renderCapture.Begin();
+
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            foreach (CModel model in models)
+                if (camera.BoundingVolumeIsInView(model.BoundingSphere))
+                    model.Draw(camera.View, camera.Projection, ((FreeCamera)camera).Position);
+
+            renderCapture.End();
+
+            GraphicsDevice.Clear(Color.Black);
+
+            postprocessor.Input = renderCapture.GetTexture();
+            postprocessor.Draw();
+            
+            base.Draw(gameTime);
+        }
+    }
+}
