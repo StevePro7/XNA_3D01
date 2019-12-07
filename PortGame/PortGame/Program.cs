2d19987de2af912212c58a1b2d2f3db759fbe091
StevePro7 <steveprozune@hotmail.com>
2019-12-07 11:15:35 +0000

    no message

--- PortGame/PortGame/Program.cs
+++ PortGame/PortGame/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using Microsoft.Xna.Framework;
 
-namespace PortGame
+namespace MyGame
 {
 #if WINDOWS || LINUX
     /// <summary>
@@ -14,9 +18,37 @@ namespace PortGame
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            int index = Convert.ToInt32(ConfigurationManager.AppSettings["game"]);
+            IDictionary<int, Game> dictionary = GetGameDictionary();
+
+            Game game = null;
+            try
+            {
+                game = dictionary[index];
+                game.Window.Title = $"Demo{index}Game";
+                game.IsMouseVisible = true;
                 game.Run();
+            }
+            finally
+            {
+                if(null != game)
+                {
+                    game.Dispose();
+                }
+            }
+        }
+
+        private static IDictionary<int, Game> GetGameDictionary()
+        {
+            IDictionary<int, Game> dictionary = new Dictionary<int, Game>();
+
+            // Chap01
+            dictionary.Add(11, new Demo11Game1());
+
+            return dictionary;
         }
     }
+
+    
 #endif
-}
+    }
