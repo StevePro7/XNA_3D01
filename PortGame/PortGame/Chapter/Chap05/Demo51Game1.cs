1232dabf80f20a1ef2bbc5c014bd2f0c510e44bb
StevePro7 <steveprozune@hotmail.com>
2019-12-12 14:59:15 +0000

    no message

--- PortGame/PortGame/Chapter/Chap05/Demo51Game1.cs
+++ PortGame/PortGame/Chapter/Chap05/Demo51Game1.cs
@@ -42,7 +42,7 @@ namespace MyGame
             //models.Add(new CModel(Content.Load<Model>("ground"),
             //    Vector3.Zero, Vector3.Zero, Vector3.One, GraphicsDevice));
 
-            Effect effect = Content.Load<Effect>("Fog");
+            Effect effect = Content.Load<Effect>("Content/Fog");
 
             models[0].SetModelEffect(effect, true);
             //models[1].SetModelEffect(effect, true);
