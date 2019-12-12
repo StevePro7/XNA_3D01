2c7b89abba6a03f68ced3b3e8a11d12f6a656c53
StevePro7 <steveprozune@hotmail.com>
2019-12-12 20:23:03 +0000

    no message

--- TestGame/01/MyGame.WIN/MyGame.WIN/Game1.cs
+++ TestGame/01/MyGame.WIN/MyGame.WIN/Game1.cs
@@ -1,83 +1,101 @@
-﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
 namespace MyGame.WIN
 {
-	/// <summary>
-	/// This is the main type for your game.
-	/// </summary>
-	public class Game1 : Game
-	{
-		GraphicsDeviceManager graphics;
-		SpriteBatch spriteBatch;
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
 
-		public Game1()
-		{
-			graphics = new GraphicsDeviceManager(this);
-			Content.RootDirectory = "Content";
-		}
+        Model model;
+        Matrix[] modelTransforms;
+        Texture2D image;
 
-		/// <summary>
-		/// Allows the game to perform any initialization it needs to before starting to run.
-		/// This is where it can query for any required services and load any non-graphic
-		/// related content.  Calling base.Initialize will enumerate through any components
-		/// and initialize them as well.
-		/// </summary>
-		protected override void Initialize()
-		{
-			// TODO: Add your initialization logic here
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            //Content.RootDirectory = "Content";
 
-			base.Initialize();
-		}
+            graphics.PreferredBackBufferWidth = 1280;
+            graphics.PreferredBackBufferHeight = 800;
+        }
 
-		/// <summary>
-		/// LoadContent will be called once per game and is the place to load
-		/// all of your content.
-		/// </summary>
-		protected override void LoadContent()
-		{
-			// Create a new SpriteBatch, which can be used to draw textures.
-			spriteBatch = new SpriteBatch(GraphicsDevice);
+        protected override void Initialize()
+        {
+            IsMouseVisible = true;
+            base.Initialize();
+        }
 
-			// TODO: use this.Content to load your game content here
-		}
+        // Called when the game should load its content
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
 
-		/// <summary>
-		/// UnloadContent will be called once per game and is the place to unload
-		/// game-specific content.
-		/// </summary>
-		protected override void UnloadContent()
-		{
-			// TODO: Unload any non ContentManager content here
-		}
+            model = Content.Load<Model>("Content/ship__cv1");
+            image = Content.Load<Texture2D>("Content/ship_tex");
 
-		/// <summary>
-		/// Allows the game to run logic such as updating the world,
-		/// checking for collisions, gathering input, and playing audio.
-		/// </summary>
-		/// <param name="gameTime">Provides a snapshot of timing values.</param>
-		protected override void Update(GameTime gameTime)
-		{
-			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-				Exit();
+            modelTransforms = new Matrix[model.Bones.Count];
+            model.CopyAbsoluteBoneTransformsTo(modelTransforms);
+        }
 
-			// TODO: Add your update logic here
+        // Called when the game should update itself
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
+                Exit();
+            }
 
-			base.Update(gameTime);
-		}
+            base.Update(gameTime);
+        }
 
-		/// <summary>
-		/// This is called when the game should draw itself.
-		/// </summary>
-		/// <param name="gameTime">Provides a snapshot of timing values.</param>
-		protected override void Draw(GameTime gameTime)
-		{
-			GraphicsDevice.Clear(Color.CornflowerBlue);
+        // Called when the game should draw itself
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
 
-			// TODO: Add your drawing code here
+            Matrix view = Matrix.CreateLookAt(
+                new Vector3(200, 300, 900),
+                new Vector3(0, 50, 0),
+                Vector3.Up);
 
-			base.Draw(gameTime);
-		}
-	}
+            Matrix projection = Matrix.CreatePerspectiveFieldOfView(
+                MathHelper.ToRadians(45), GraphicsDevice.Viewport.AspectRatio,
+                0.1f, 10000.0f);
+
+            // Calculate the starting world matrix
+            Matrix baseWorld = Matrix.CreateScale(0.4f) *
+                Matrix.CreateRotationY(MathHelper.ToRadians(180));
+
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                // Calculate each mesh's world matrix
+                Matrix localWorld = modelTransforms[mesh.ParentBone.Index]
+                    * baseWorld;
+
+                foreach (ModelMeshPart part in mesh.MeshParts)
+                {
+                    BasicEffect e = (BasicEffect)part.Effect;
+
+                    // Set the world, view, and projection 
+                    // matrices to the effect
+                    e.World = localWorld;
+                    e.View = view;
+                    e.Projection = projection;
+                    //http://community.monogame.net/t/problem-with-3d-objects-textures/2474/12
+                    e.Texture = image;
+                    //e.TextureEnabled = false;
+                    e.EnableDefaultLighting();
+                }
+
+                // Draw the mesh
+                mesh.Draw();
+            }
+
+            base.Draw(gameTime);
+        }
+    }
 }
