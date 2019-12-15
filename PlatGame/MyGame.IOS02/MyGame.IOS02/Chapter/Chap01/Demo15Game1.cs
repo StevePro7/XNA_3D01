6b233217c32b771e98e0c70c91ef8d33e9df9547
StevePro7 <steveprozune@hotmail.com>
2019-12-15 16:10:27 +0000

    no message

---
+++ PlatGame/MyGame.IOS02/MyGame.IOS02/Chapter/Chap01/Demo15Game1.cs
@@ -0,0 +1,105 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Input;
+
+namespace MyGame
+{
+    public class Demo15Game1 : MyBaseGame
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        List<CModel> models = new List<CModel>();
+        Camera camera;
+
+        MouseState lastMouseState;
+
+		public Demo15Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            //Content.RootDirectory = "Content";
+
+            graphics.PreferredBackBufferWidth = 1280;
+            graphics.PreferredBackBufferHeight = 800;
+        }
+
+        // Called when the game should load its content
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            models.Add(new CModel(Content.Load<Model>("Content/ship__cv1"),
+                new Vector3(0, 400, 0), Vector3.Zero, new Vector3(0.4f), GraphicsDevice));
+
+            models.Add(new CModel(Content.Load<Model>("Content/ground"),
+                Vector3.Zero, Vector3.Zero, Vector3.One, GraphicsDevice, false));
+
+            camera = new ChaseCamera(new Vector3(0, 400, 1500), new Vector3(0, 200, 0),
+                new Vector3(0, 0, 0), GraphicsDevice);
+
+            lastMouseState = Mouse.GetState();
+        }
+
+        // Called when the game should update itself
+        protected override void Update(GameTime gameTime)
+        {
+            updateModel(gameTime);
+            updateCamera(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        void updateModel(GameTime gameTime)
+        {
+            KeyboardState keyState = Keyboard.GetState();
+
+            Vector3 rotChange = new Vector3(0, 0, 0);
+
+            // Determine on which axes the ship should be rotated on, if any
+            if (keyState.IsKeyDown(Keys.W))
+                rotChange += new Vector3(1, 0, 0);
+            if (keyState.IsKeyDown(Keys.S))
+                rotChange += new Vector3(-1, 0, 0);
+            if (keyState.IsKeyDown(Keys.A))
+                rotChange += new Vector3(0, 1, 0);
+            if (keyState.IsKeyDown(Keys.D))
+                rotChange += new Vector3(0, -1, 0);
+
+            models[0].Rotation += rotChange * .025f;
+
+            // If space isn't down, the ship shouldn't move
+            if (!keyState.IsKeyDown(Keys.Space))
+                return;
+
+            // Determine what direction to move in
+            Matrix rotation = Matrix.CreateFromYawPitchRoll(
+                models[0].Rotation.Y, models[0].Rotation.X, models[0].Rotation.Z);
+
+            // Move in the direction dictated by our rotation matrix
+            models[0].Position += Vector3.Transform(Vector3.Forward, rotation)
+                * (float)gameTime.ElapsedGameTime.TotalMilliseconds * 4;
+        }
+
+        void updateCamera(GameTime gameTime)
+        {
+            // Move the camera to the new model's position and orientation
+            ((ChaseCamera)camera).Move(models[0].Position, models[0].Rotation);
+
+            // Update the camera
+            camera.Update();
+        }
+
+        // Called when the game should draw itself
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            foreach (CModel model in models)
+                if (camera.BoundingVolumeIsInView(model.BoundingSphere))
+                    model.Draw(camera.View, camera.Projection);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ PlatGame/MyGame.IOS02/MyGame.IOS02/Chapter/Chap06/Demo63Game1.cs
@@ -0,0 +1,148 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace MyGame
+{
+    public class Demo63Game1 : MyBaseGame
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        List<CModel> models = new List<CModel>();
+        Camera camera;
+
+        MouseState lastMouseState;
+
+        ParticleSystem ps;
+        ParticleSystem smoke;
+        Random r = new Random();
+
+		public Demo63Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            //Content.RootDirectory = "Content";
+
+            graphics.PreferredBackBufferWidth = 1280;
+            graphics.PreferredBackBufferHeight = 800;
+        }
+
+        // Called when the game should load its content
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            models.Add(new CModel(Content.Load<Model>("Content/ground"),
+                Vector3.Zero, Vector3.Zero, Vector3.One, GraphicsDevice, false));
+
+            Effect effect = Content.Load<Effect>("Content/LightingEffect");
+
+            LightingMaterial mat = new LightingMaterial();
+            mat.SpecularColor = Color.Black.ToVector3();
+
+            //models[0].SetModelEffect(effect, true);
+            //models[0].Material = mat;
+            
+            camera = new FreeCamera(new Vector3(0, 700, 3000),
+                MathHelper.ToRadians(0),
+                MathHelper.ToRadians(5),
+                GraphicsDevice);
+
+            ps = new ParticleSystem(GraphicsDevice, Content, Content.Load<Texture2D>("Content/fire"), 
+                400, new Vector2(400), 1, Vector3.Zero, 0.5f);
+
+            smoke = new ParticleSystem(GraphicsDevice, Content, Content.Load<Texture2D>("Content/smoke"),
+                400, new Vector2(800), 6, new Vector3(500, 0, 0), 5f);
+
+            lastMouseState = Mouse.GetState();
+        }
+
+        // Called when the game should update itself
+        protected override void Update(GameTime gameTime)
+        {
+            updateCamera(gameTime);
+
+            // Generate a direction within 15 degrees of (0, 1, 0)
+            Vector3 offset = new Vector3(MathHelper.ToRadians(10.0f));
+            Vector3 randAngle = Vector3.Up + randVec3(-offset, offset);
+                
+            // Generate a position between (-400, 0, -400) and (400, 0, 400)
+            Vector3 randPosition = randVec3(new Vector3(-400), new Vector3(400));
+
+            // Generate a speed between 600 and 900
+            float randSpeed = (float)r.NextDouble() * 300 + 600;
+
+            ps.AddParticle(randPosition, randAngle, randSpeed);
+            ps.Update();
+
+            smoke.AddParticle(randPosition + new Vector3(0, 1200, 0), randAngle, randSpeed);
+            smoke.Update();
+
+            base.Update(gameTime);
+        }
+
+        // Returns a random Vector3 between min and max
+        Vector3 randVec3(Vector3 min, Vector3 max)
+        {
+            return new Vector3(
+                min.X + (float)r.NextDouble() * (max.X - min.X),
+                min.Y + (float)r.NextDouble() * (max.Y - min.Y),
+                min.Z + (float)r.NextDouble() * (max.Z - min.Z));
+        }
+
+        void updateCamera(GameTime gameTime)
+        {
+            // Get the new keyboard and mouse state
+            MouseState mouseState = Mouse.GetState();
+            KeyboardState keyState = Keyboard.GetState();
+
+            // Determine how much the camera should turn
+            float deltaX = (float)lastMouseState.X - (float)mouseState.X;
+            float deltaY = (float)lastMouseState.Y - (float)mouseState.Y;
+
+            // Rotate the camera
+            ((FreeCamera)camera).Rotate(deltaX * .005f, deltaY * .005f);
+
+            Vector3 translation = Vector3.Zero;
+
+            // Determine in which direction to move the camera
+            if (keyState.IsKeyDown(Keys.W)) translation += Vector3.Forward;
+            if (keyState.IsKeyDown(Keys.S)) translation += Vector3.Backward;
+            if (keyState.IsKeyDown(Keys.A)) translation += Vector3.Left;
+            if (keyState.IsKeyDown(Keys.D)) translation += Vector3.Right;
+
+            // Move 4 units per millisecond, independent of frame rate
+            translation *= 4 * 
+                (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            // Move the camera
+            ((FreeCamera)camera).Move(translation);
+
+            // Update the camera
+            camera.Update();
+
+            // Update the mouse state
+            lastMouseState = mouseState;
+        }
+
+        // Called when the game should draw itself
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            foreach (CModel model in models)
+                if (camera.BoundingVolumeIsInView(model.BoundingSphere))
+                    model.Draw(camera.View, camera.Projection, ((FreeCamera)camera).Position);
+
+            ps.Draw(camera.View, camera.Projection, 
+                ((FreeCamera)camera).Up, ((FreeCamera)camera).Right);
+
+            smoke.Draw(camera.View, camera.Projection,
+                ((FreeCamera)camera).Up, ((FreeCamera)camera).Right);
+            
+            base.Draw(gameTime);
+        }
+    }
+}
