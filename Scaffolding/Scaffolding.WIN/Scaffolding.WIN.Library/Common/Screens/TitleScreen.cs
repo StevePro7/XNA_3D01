b21e65f09a555fe39aab65b978d0d858154a6c37
StevePro7 <steveprozune@hotmail.com>
2019-12-16 16:56:04 +0000

    no message

---
+++ Scaffolding/Scaffolding.WIN/Scaffolding.WIN.Library/Common/Screens/TitleScreen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using WindowsGame.Common.Interfaces;
+using WindowsGame.Common.Static;
+using WindowsGame.Master;
+
+namespace WindowsGame.Common.Screens
+{
+	public class TitleScreen : BaseScreen, IScreen
+	{
+		public override void Initialize()
+		{
+			base.Initialize();
+		}
+
+		public override void LoadContent()
+		{
+			base.LoadContent();
+		}
+
+		public ScreenType Update(GameTime gameTime)
+		{
+			return ScreenType.Title;
+		}
+
+		public override void Draw()
+		{
+            //Engine.SpriteBatch.Draw(Assets.SplashTexture, BannerPosition, Color.White);
+        }
+
+	}
+}
