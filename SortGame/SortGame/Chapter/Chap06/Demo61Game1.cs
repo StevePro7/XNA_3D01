2e72601bd1c65fc135a89c41cf40ee07a246a1a4
StevePro7 <steveprozune@hotmail.com>
2020-07-25 19:53:08 +0100

    no message

---
+++ SortGame/SortGame/Chapter/Chap06/Demo61Game1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace MyGame.Chapter.Chap06
+{
+    public class Demo61Game1 : MyBaseGame
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        List<CModel> models = new List<CModel>();
+        Camera camera;
+
+        MouseState lastMouseState;
+
+        
+    }
+}
