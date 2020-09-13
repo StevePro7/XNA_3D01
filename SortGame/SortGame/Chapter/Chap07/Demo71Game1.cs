199e2729f01ade2ad92ea430ef03f1ee50a10a86
StevePro7 <steveprozune@hotmail.com>
2020-09-13 20:00:52 +0100

    no message

---
+++ SortGame/SortGame/Chapter/Chap07/Demo71Game1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+
+namespace MyGame
+{
+	public class Demo71Game1 : MyBaseGame
+	{
+	}
+}
